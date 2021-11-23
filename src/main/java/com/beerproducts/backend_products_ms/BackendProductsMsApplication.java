43c698856744ffc6d95e798832a460f1cad6c122
KuroMicho <kevin0552009.kr@gmail.com>
2021-11-23 14:03:06 -0500

    initialzr boilerplate product service

---
+++ src/main/java/com/beerproducts/backend_products_ms/BackendProductsMsApplication.java
@@ -0,0 +1,13 @@
+package com.beerproducts.backend_products_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProductsMsApplication.class, args);
+	}
+
+}
